21535180a9e68f6476c8a4293fa4e57433f7a5cf
William Wilson <w0457754@campus.nscc.ca>
2026-03-11 12:32:40 -0300

    fixed spelling error

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
     auth: {
         clientId: "397d85f9-6314-4b12-8efa-d43430c38876",
-        authority: "https://login.microsfotonline.com/0dc25d87-cd68-49b9-91ae-0ffc56d1eb24",
+        authority: "https://login.microsoftonline.com/0dc25d87-cd68-49b9-91ae-0ffc56d1eb24",
         redirectUri: "http://localhost:5173", //This MUST match the URI in Azure
     },
     cache: {
